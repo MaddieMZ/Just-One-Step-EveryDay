f8285ba81008c9025f5e4987f249a6d7a9283d9e
MaddieMZ <josemarin191@gmail.com>
2023-05-28 19:58:48 -0600

    Added Exercises full size assets
    Added exercise assets used during workout time

--- src/Interfaces/Contenido.java
+++ src/Interfaces/Contenido.java
@@ -11,5 +11,16 @@ public class Contenido extends JPanel{
 		setLayout(null);
 		setBounds(0,0,700,740);
 		setVisible(true);
+		JLabel ej1i= new JLabel("");
+		JLabel ej2i= new JLabel("");
+		JLabel ej3i= new JLabel("");
+		JLabel ej4i= new JLabel("");
+		JLabel ej5i= new JLabel("");
+		JLabel ej6i= new JLabel("");
+		JLabel ej7i= new JLabel("");
+		JLabel ej8i= new JLabel("");
+		JLabel ej9i= new JLabel("");
+		JLabel ej10i= new JLabel("");
+		JLabel ej11i= new JLabel("");
 	}
 }
